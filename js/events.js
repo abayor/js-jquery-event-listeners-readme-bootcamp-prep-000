42e510201f483af8aafe368b0d0b0d2fde366ab3
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-17 22:12:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -29,7 +29,7 @@
   function submitIt(){
     $('form').on('submit', function submitIt(){
   
-    alert("Your form is going to be submitted now");
+    alert("Your form is going to be submitted now.");
   
 })
 
